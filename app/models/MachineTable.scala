6251102d4e010bec80d3a3d89c8e3a5670112fe9
Phillip Thelen <phillip@thelen.space>
2023-05-19 15:01:03 +0200

    Change machine name to hostname and comment to name

--- app/models/MachineTable.scala
+++ app/models/MachineTable.scala
@@ -6,22 +6,22 @@ import Index.machineTableIndex
 /** Convenience class for a Machine. */
 case class Machine(
   id: Option[Int],
-  name: String,
+  hostname: String,
   macAddress: String,
-  comment: Option[String],
+  name: Option[String],
   isActive: Boolean
 )
 
 class MachineTable(tag: Tag) extends Table[Machine](tag, "tb_machine") {
 
   def id: Rep[Int] = column[Int]("pk_id", O.PrimaryKey, O.AutoInc)
-  def name: Rep[String] = column[String]("dt_name")
+  def hostname: Rep[String] = column[String]("dt_name")
   def macAddress: Rep[String] = column[String]("dt_mac_address")
-  def comment: Rep[Option[String]] = column[Option[String]]("dt_comment")
+  def name: Rep[Option[String]] = column[Option[String]]("dt_comment")
   def isActive: Rep[Boolean] = column[Boolean]("dt_active")
 
   def * = {
-    (id.?, name, macAddress, comment, isActive) <> (Machine.tupled, Machine.unapply)
+    (id.?, hostname, macAddress, name, isActive) <> (Machine.tupled, Machine.unapply)
   }
 
   def idx = {
