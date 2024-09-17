0564f0a10dc8fbfd1289594ec7ceda97e6eb4276
Phillip Thelen <phillip@thelen.space>
2024-09-17 18:36:30 +0200

    Improve access log listing

--- access_log/urls.py
+++ access_log/urls.py
@@ -7,8 +7,18 @@ app_name = "access_log"
 urlpatterns = [
     path("", views.AccessLogListView.as_view(), name="list"),
     path(
-        "for_token/<int:token>",
-        views.AccessLogForTokenSnippet.as_view(),
+        "token/<int:token>",
+        views.AccessLogForTokenView.as_view(),
         name="for-token",
+    ),
+path(
+        "person/<int:person>",
+        views.AccessLogForPersonView.as_view(),
+        name="for-person",
+    ),
+    path(
+        "machine/<int:machine>",
+        views.AccessLogForMachineView.as_view(),
+        name="for-machine",
     ),
 ]
--- access_log/views.py
+++ access_log/views.py
@@ -4,7 +4,8 @@ from django.views.generic import ListView
 
 from .models import AccessLog
 from tokens.models import Token
-
+from machines.models import Machine
+from people.models import Person
 
 class AccessLogListView(ListView):
     model = AccessLog
@@ -25,7 +26,7 @@ class AccessLogListView(ListView):
         return context
 
 
-class AccessLogForTokenSnippet(ListView):
+class AccessLogForTokenView(ListView):
     model = AccessLog
     context_object_name = "access_logs"
     ordering = ["-timestamp"]
@@ -47,4 +48,56 @@ class AccessLogForTokenSnippet(ListView):
         context = super().get_context_data(**kwargs)
         context["model"] = self.model
         context["token"] = Token.objects.get(pk=self.kwargs["token"])
+        context["is_partial"] = self.request.headers.get("HX-Request") == "true"
+        return context
+
+class AccessLogForPersonView(ListView):
+    model = AccessLog
+    context_object_name = "access_logs"
+    ordering = ["-timestamp"]
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().select_related("machine").filter(token__person__pk=self.kwargs["person"])
+
+    def get_template_names(self) -> list[str]:
+        is_htmx = self.request.headers.get("HX-Request") == "true"
+        if is_htmx:
+            return ["partial_access_log_for_person.html"]
+        else:
+            return ["access_log_for_person.html"]
+
+    def get_paginate_by(self, queryset):
+        return self.request.user.page_length
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["model"] = self.model
+        context["person"] = Person.objects.get(pk=self.kwargs["person"])
+        context["is_partial"] = self.request.headers.get("HX-Request") == "true"
+        return context
+
+
+class AccessLogForMachineView(ListView):
+    model = AccessLog
+    context_object_name = "access_logs"
+    ordering = ["-timestamp"]
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(machine__pk=self.kwargs["machine"])
+
+    def get_template_names(self) -> list[str]:
+        is_htmx = self.request.headers.get("HX-Request") == "true"
+        if is_htmx:
+            return ["partial_access_log_for_machine.html"]
+        else:
+            return ["access_log_for_machine.html"]
+
+    def get_paginate_by(self, queryset):
+        return self.request.user.page_length
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["model"] = self.model
+        context["machine"] = Machine.objects.get(pk=self.kwargs["machine"])
+        context["is_partial"] = self.request.headers.get("HX-Request") == "true"
         return context
