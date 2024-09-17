250be8de79795d432d9dd79a96a5f388d626a24b
Phillip Thelen <phillip@thelen.space>
2024-09-17 17:24:13 +0200

    string fix

--- space/models.py
+++ space/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 from base.models import TimestampedModel
 
@@ -8,7 +9,7 @@ class SpaceState(TimestampedModel):
     is_open = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"Space is {'open' if self.is_open else 'closed'}"
+        return _("Open") if self.is_open else _("Closed")
 
     class Meta:
         ordering = ["-created"]
