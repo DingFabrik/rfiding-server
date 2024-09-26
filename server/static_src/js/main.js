c388c489d061b89436b6b89d6df947e45986c1f8
Phillip Thelen <phillip@thelen.space>
2024-09-26 18:09:01 +0200

    fix tooltips/popovers on htmx loads

--- server/static_src/js/main.js
+++ server/static_src/js/main.js
@@ -6,5 +6,32 @@ require("jquery-ui/dist/jquery-ui.js");
 
 window.bootstrap = bs
 
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bs.Tooltip(tooltipTriggerEl))
+window.urlMap = {
+
+};
+
+window.updateTooltips = function () {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bs.Tooltip(tooltipTriggerEl))
+
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="token-popover"]')
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bs.Popover(popoverTriggerEl, {
+        "html": true,
+        "delay": 200,
+        "content": function (e) {
+            const token_pk = e.getAttribute('data-token-pk');
+            $.ajax({
+                url: window.urlMap['person-for-token-popover'] + '?token_pk=' + token_pk,
+                success: function(response){
+                    jquery('#'+token_pk).html(response);
+                }
+            });
+            return '<div id="'+ token_pk +'">Loading...</div>';
+        }
+    }));
+}
+
+window.addEventListener('htmx:afterSwap', function(event) {
+    console.log(event);
+    updateTooltips();
+})
