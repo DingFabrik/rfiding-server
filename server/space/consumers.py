2c3314afaef947270241eea1459d810d551d8755
Phillip Thelen <phillip@thelen.space>
2024-12-04 14:37:41 +0100

    improve ping/pong handling

--- server/space/consumers.py
+++ server/space/consumers.py
@@ -20,7 +20,8 @@ class SpaceStateConsumer(AsyncJsonWebsocketConsumer):
         return super().disconnect(code)
     
     async def receive(self, text_data=None, bytes_data=None):
-        if text_data == None or text_data == "":
+        if (text_data == None or text_data == "" or
+            text_data.lower() == "ping" or text_data.lower() == "pong"):
             return
         json_data = json.loads(text_data)
         
