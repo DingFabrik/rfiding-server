bf5fbdbbd8336364ec5f06637193dd8f79f434a3
Phillip Thelen <phillip@thelen.space>
2025-01-03 11:58:02 +0100

    Add more logging for websocket events

--- server/space/consumers.py
+++ server/space/consumers.py
@@ -2,10 +2,12 @@ import json
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from channels.layers import get_channel_layer
 from django.conf import settings as SETTINGS
+import logging
 
 from .common import aupdate_space_state, aget_current_space_state
 
 channel_layer = get_channel_layer()
+logger = logging.getLogger(__name__)
 
 class SpaceStateConsumer(AsyncJsonWebsocketConsumer):
     
@@ -14,12 +16,15 @@ class SpaceStateConsumer(AsyncJsonWebsocketConsumer):
         await self.accept()
         
         await self.send(json.dumps({"state": (await aget_current_space_state()).is_open}))
+        logger.debug("Websocket client connected")
     
     async def disconnect(self, code):
-        channel_layer.group_discard("space_state", self.channel_name)
-        return super().disconnect(code)
+        await channel_layer.group_discard("space_state", self.channel_name)
+        logger.debug("websocket client disconnected")
+        return await super().disconnect(code)
     
     async def receive(self, text_data=None, bytes_data=None):
+        logger.debug("Received websocket data:", text_data)
         if (text_data is None or text_data == "" or
             text_data.lower() == "ping" or text_data.lower() == "pong"):
             return
@@ -36,4 +41,5 @@ class SpaceStateConsumer(AsyncJsonWebsocketConsumer):
             await self.send(json.dumps({"error": "invalid method"}))
             
     async def space_state(self, event):
+        logger.debug("Sending new state through websocket")
         await self.send(json.dumps({"state": event["state"]}))
