a9658f708a4c761719449659a6a8e093020ca778
Phillip Thelen <phillip@thelen.space>
2025-02-20 20:44:02 +0100

    hide is_staff from form

--- server/users/forms.py
+++ server/users/forms.py
@@ -7,7 +7,7 @@ class UserForm(forms.ModelForm):
     
     class Meta:
         model = RFIDingUser
-        fields = ["name", "email", "is_superuser", "is_staff", "is_active", "groups", "user_permissions"]
+        fields = ["name", "email", "is_superuser", "is_active", "groups", "user_permissions"]
         widgets = {
             "user_permissions": forms.SelectMultiple(attrs={"size":"17"}),
         }
