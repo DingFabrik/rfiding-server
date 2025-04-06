233fffcf18135a96bad984a746935b989e8a9b25
Phillip Thelen <phillip@thelen.space>
2025-04-07 00:19:24 +0200

    Improve user form display

--- server/users/forms.py
+++ server/users/forms.py
@@ -1,10 +1,47 @@
 from django import forms
 from django.contrib.auth.models import Group
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, Submit, HTML
+from crispy_forms.bootstrap import AppendedText, FormActions
+from django.utils.translation import gettext as _
 
 from .models import RFIDingUser
 
 
 class UserForm(forms.ModelForm):
+    
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.layout = Layout(
+            "name",
+            "email",
+            "is_active",
+            Fieldset(
+                _("Permissions"),
+                "is_superuser",
+                "groups",
+                "user_permissions",
+                css_class="border rounded p-2 mb-3",
+            ),
+            "last_login",
+            "date_joined",
+            FormActions(
+                Submit("submit", _("Save")),
+                HTML("""{% load i18n %}
+                     {% if object and request.user.is_superuser %}
+    <a class="btn btn-warning float-end me-3" href="{% url 'users:admin_change_password' object.pk %}">
+        <i class="bi-key me-1"></i> {% trans 'Change Password' %}
+    </a>
+{% endif %}
+                     {% if object and can_delete %}
+            <a class="btn btn-danger float-end" href="{% url request.resolver_match.namespace|add:':delete' object.pk %}">
+                <i class="bi-trash me-1"></i> {% trans 'Delete' %}
+            </a>
+        {% endif %}"""),
+            ),
+        )
+    
     class Meta:
         model = RFIDingUser
         fields = [
@@ -14,6 +51,8 @@ class UserForm(forms.ModelForm):
             "is_active",
             "groups",
             "user_permissions",
+            "last_login",
+            "date_joined",
         ]
         widgets = {
             "user_permissions": forms.SelectMultiple(attrs={"size": "17"}),
